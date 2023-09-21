91758b0f60b07a418c549748b72a5257c2dc2c48
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:17:52 +0000

    Backup Commit

--- FILE.tf
+++ FILE.tf
@@ -12,7 +12,6 @@ terraform {
     resource "aws_instance" "FILE"{
         ami="SUSELinuxEnterpriseServer12 SP5"
         instance_type="t2.micro"
-        print Auto-Assigned Public-IP
     }
     output "instance_public_ip"{
         value=aws_instance.FILE.public_ip
