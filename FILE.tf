905b4d3df8c7d7218acf01c2886df7f696c03de5
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:59:41 +0000

    Backup Commit

--- FILE.tf
+++ FILE.tf
@@ -1,4 +1,17 @@
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    aws={
+        source="hasicorp/aws"
+    }
+    provide "aws"{
+        region="Singapore"
+    }
+    resource "aws_instance" "FILE"{
+        ami=""
+    }
+    }
+    
+  }
   
 }
