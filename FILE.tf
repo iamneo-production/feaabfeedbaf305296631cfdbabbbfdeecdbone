4171902de9627c4cf1bf054b157d1f89d67e2275
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:04:41 +0000

    Backup Commit

--- FILE.tf
+++ FILE.tf
@@ -2,16 +2,20 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     aws={
-        source="hasicorp/aws"
+        source="hashicorp/aws"
     }
+  }
+}
     provide "aws"{
         region="Singapore"
     }
     resource "aws_instance" "FILE"{
-        ami=""
+        ami="SUSE Linux Enterprise Server 12 SP5"
+        instance_type="t2.micro"
+        print Auto-Assigned Public-IP
     }
     }
     
   }
   
-}
+
