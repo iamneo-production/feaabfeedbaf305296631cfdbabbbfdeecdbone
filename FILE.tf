080b96656ad0143be66670e0ee8c63d33a7b22e6
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:09:54 +0000

    Backup Commit

--- FILE.tf
+++ FILE.tf
@@ -6,16 +6,18 @@ terraform {
     }
   }
 }
-    provide "aws"{
+    provider "aws"{
         region="Singapore"
     }
     resource "aws_instance" "FILE"{
-        ami="SUSE Linux Enterprise Server 12 SP5"
+        ami="SUSELinuxEnterpriseServer12 SP5"
         instance_type="t2.micro"
         print Auto-Assigned Public-IP
     }
+    output "instance_public_ip"{
+        value=aws_instance.FILE.public_ip
     }
     
-  }
+  
   
 
